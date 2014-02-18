ec61ca9ba065f75b49e23d12eb6778aca878aaf1
Halleck45 <jeanfrancois@lepine.pro>
2014-02-18 18:26:37 +0100

    preparing 0.0.4

--- build.php
+++ build.php
@@ -34,7 +34,7 @@ $phar->setStub(<<<STUB
 Phar::mapPhar('metrics.phar');
 
 require_once 'phar://metrics.phar/vendor/autoload.php';
-\$app = new Hal\Console\PhpMetricsApplication('PhpMetrics, by Jean-François Lépine (https://twitter.com/Halleck45)', '0.0.3');
+\$app = new Hal\Console\PhpMetricsApplication('PhpMetrics, by Jean-François Lépine (https://twitter.com/Halleck45)', '0.0.4');
 \$app->run();
 
 __HALT_COMPILER();
