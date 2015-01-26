7f02b8c955ecc43d7775537f7565e3835a40dd17
Tommy Mühle <tommy.muehle@gmail.com>
2015-01-26 13:46:39 +0100

    Update SyntaxChecker.php

--- src/Hal/Component/File/SyntaxChecker.php
+++ src/Hal/Component/File/SyntaxChecker.php
@@ -26,9 +26,16 @@ class SyntaxChecker
     public function isCorrect($filename) {
         if(1 === version_compare('5.0.4', PHP_VERSION)) {
             return php_check_syntax($filename);
-        } else {
-            $output = shell_exec(sprintf('php -l %s 2>&1', escapeshellarg($filename)));
-            return preg_match('!No syntax errors detected!', $output);
         }
+
+        $php = 'php';
+
+        if (0 >= version_compare('5.4.0', PHP_VERSION)) {
+           $php = PHP_BINARY;
+        }
+
+        $output = shell_exec(sprintf('%s -l %s 2>&1', $php, escapeshellarg($filename)));
+        
+        return preg_match('!No syntax errors detected!', $output);
     }
 }
