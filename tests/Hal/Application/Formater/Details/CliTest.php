13f4ef34914a4ab7bff912fca3d2e044738ec353
Sebastian Kors <s.kors@unico-soft.com>
2016-02-14 23:52:39 +0100

    add test with data provider to check for correct calculated duration on CLI detail output

--- tests/Hal/Application/Formater/Details/CliTest.php
+++ tests/Hal/Application/Formater/Details/CliTest.php
@@ -43,4 +43,48 @@ class CliTest extends \PHPUnit_Framework_TestCase {
         $formater = new Cli($validator, $bounds);
         $this->assertNotNull($formater->getName());
     }
+
+    /**
+     * @dataProvider validSecondsToTimeString
+     */
+    public function testFormaterReturnsCorrectDurationForUnderstanding($duration, $expectedString) {
+        $collection = $this->getMockBuilder('\Hal\Component\Result\ResultCollection')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $collection->expects($this->any())
+            ->method('asArray')
+            ->will(
+                $this->returnValue(
+                    [
+                        ['time' => $duration],
+                    ]
+                )
+            );
+        $validator = $this->getMockBuilder('\Hal\Application\Rule\Validator')->disableOriginalConstructor()->getMock();
+
+        $bounds = new Bounds();
+        $formater = new Cli($validator, $bounds);
+        $groupedResults = new ResultCollection();
+        $output = $formater->terminate($collection, $groupedResults);
+        $this->assertContains($expectedString, $output);
+    }
+
+    public function validSecondsToTimeString() {
+        return [
+            // zero
+            [-1, '0 hour(s), 0 minute(s) and 1 second(s)'],
+            // zero
+            [0, '0 hour(s), 0 minute(s) and 0 second(s)'],
+            // just seconds
+            [25, '0 hour(s), 0 minute(s) and 25 second(s)'],
+            // more than a minute
+            [80, '0 hour(s), 1 minute(s) and 20 second(s)'],
+            // less than an hour
+            [3482, '0 hour(s), 58 minute(s) and 2 second(s)'],
+            // more than an hour, less than a day
+            [14532, '4 hour(s), 2 minute(s) and 12 second(s)'],
+            // more than a day
+            [132437, '36 hour(s), 47 minute(s) and 17 second(s)'],
+        ];
+    }
 }
