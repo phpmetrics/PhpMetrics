c63e1088f60d7426202412cc576de11e946edf3c
Jean-François <lepinejeanfrancois@yahoo.fr>
2016-03-22 08:06:43 +0100

    added new default ruleset object

--- src/Hal/Application/Rule/RuleSet.php
+++ src/Hal/Application/Rule/RuleSet.php
@@ -37,21 +37,8 @@ class RuleSet implements ExportableInterface {
     public function __construct(array $rules = array())
     {
         if(!$rules) {
-            $rules = array(
-                'cyclomaticComplexity' => array(10, 6, 2)
-                , 'maintainabilityIndex' => array(0, 69, 85)
-                , 'logicalLoc' => array(800, 400, 200)
-                , 'volume' => array(1300, 1000, 300)
-                , 'bugs' => array(0.35, 0.25, 0.15)
-                , 'commentWeight' => array(36, 38, 41)
-                , 'vocabulary' => array(51, 34, 27)
-                , 'difficulty' => array(18, 15, 5.8)
-                , 'instability' => array(1, .95, .45)
-                , 'afferentCoupling' => array(20, 15, 9)
-                , 'efferentCoupling' => array(15, 11, 7)
-                , 'myerDistance' => array(10, 5, 2)
-                , 'lcom' => array(3, 2, 1.5)
-            );
+            $default = new DefaultRuleSet();
+            $rules = $default->asArray();
         }
 
         $this->rules = $rules;
