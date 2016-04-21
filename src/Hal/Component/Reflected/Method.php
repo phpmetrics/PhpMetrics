2f852328a5f364234d89794bad80e269d0e85c32
Jean-François <lepinejeanfrancois@yahoo.fr>
2016-04-21 13:27:40 +0200

    fixed license header

--- src/Hal/Component/Reflected/Method.php
+++ src/Hal/Component/Reflected/Method.php
@@ -1,6 +1,13 @@
 <?php
-namespace Hal\Component\Reflected;
 
+/*
+ * (c) Jean-François Lépine <https://twitter.com/Halleck45>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hal\Component\Reflected;
 
 use Hal\Component\Parser\Helper\TypeResolver;
 use Hal\Component\Token\Token;
