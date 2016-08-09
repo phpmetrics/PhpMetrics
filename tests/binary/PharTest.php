53c4c8478f206a1b383cd9bbf9fe8268c344c786
Jean-François <lepinejeanfrancois@yahoo.fr>
2016-08-09 18:20:54 +0200

    added some functional tests for Phar artifact

---
+++ tests/binary/PharTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Test\Binary;
+
+/**
+ * @group binary
+ */
+class PharTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $phar = __DIR__ . '/../binary/../../build/phpmetrics.phar';
+
+    public function testICanRunPhar()
+    {
+        $command = sprintf('%s --version', $this->phar);
+        $r = shell_exec($command);
+        $this->assertContains('PhpMetrics', $r);
+    }
+
+    public function testICanProvideOneDirectoryToParse()
+    {
+        $command = sprintf('%s %s 2>&1', $this->phar, __DIR__.'/examples/1');
+        $r = shell_exec($command);
+        $this->assertContains('Object oriented programming', $r);
+        $this->assertContains('LOC', $r);
+        $this->assertRegExp('!Classes\s+2!', $r);
+    }
+
+    public function testICanProvideMultipleDirectoriesToParse()
+    {
+        $command = sprintf('%s  %s,%s  2>&1', $this->phar, __DIR__.'/examples/1', __DIR__.'/examples/2');
+        $r = shell_exec($command);
+        $this->assertContains('Object oriented programming', $r);
+        $this->assertContains('LOC', $r);
+        $this->assertRegExp('!Classes\s+4!', $r);
+    }
+}
