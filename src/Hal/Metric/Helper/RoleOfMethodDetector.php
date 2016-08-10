0bc46e02c03c7c2971a0d1386d0441df2e35f0e9
Jean-François <lepinejeanfrancois@yahoo.fr>
2016-08-10 18:09:01 +0200

    tool to detect role of method (getter, setter)

---
+++ src/Hal/Metric/Helper/RoleOfMethodDetector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jean-francois
+ * Date: 10/08/16
+ * Time: 17:49
+ */
+
+namespace Hal\Metric\Helper;
+
+
+class RoleOfMethodDetector
+{
+
+}
---
+++ tests/Metric/Helper/RoleOfMethodDetectorTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace Test\Hal\Metric\Helper;
+
+use Hal\Metric\Helper\RoleOfMethodDetector;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\ParserFactory;
+
+/**
+ * @group method
+ * @group helper
+ * @group parsing
+ */
+class RoleOfMethodDetectorTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider provideExamples
+     */
+    public function testICanDetectRoleOfMethod($expected, $code)
+    {
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        $stmt = $parser->parse($code);
+
+        $helper = new RoleOfMethodDetector();
+
+        foreach ($stmt as $node) {
+            if ($node instanceof Class_) {
+                foreach ($node->stmts as $sub) {
+                    if ($sub instanceof ClassMethod) {
+                        $type = $helper->detects($sub);
+                        $this->assertEquals($expected, $type);
+                    }
+                }
+            }
+        }
+    }
+
+    public function provideExamples()
+    {
+        return [
+            ['getter', '<?php class A { function getName(){ return $this->name; } }  ?>'],
+            ['getter', '<?php class A { function getName(){ return (string) $this->name; } }  ?>'],
+            ['getter', '<?php class A { function getName(){ return (int) $this->name; } }  ?>'],
+            ['setter', '<?php class A { function setName($string){ $this->name = $name; } } ?>'],
+            ['setter', '<?php class A { function setName($string){ $this->name = (string) $name; } } ?>'],
+            ['setter', '<?php class A { function setName($string){ $this->name = (string) $name; return $this; } } ?>'],
+            /*[null, '<?php class A { function foo($string){ $this->name = (string) $name * 3; } } ?>'],*/
+
+        ];
+    }
+}
