7f51e79ea5decfaff4aebbfc01c319ca767eacd6
Jean-François <lepinejeanfrancois@yahoo.fr>
2017-04-03 18:17:38 +0200

    improved unit testing report

--- src/Hal/Metric/System/UnitTesting/UnitTesting.php
+++ src/Hal/Metric/System/UnitTesting/UnitTesting.php
@@ -52,6 +52,7 @@ class UnitTesting
 
         $unitsTests = [];
         $infoAboutTests = [];
+        $assertions = 0;
         $projectMetric = new ProjectMetric('unitTesting');
 
         // injects default value for each metric
@@ -60,18 +61,36 @@ class UnitTesting
         }
 
         // parsing of XML file without any dependency to DomDocument or simpleXML
-        // we want to be compatible with every platforms
+        // we want to be compatible with every platforms. Maybe (probably) that's a really stupid idea, but I want to try it :p
+        $testsuites = [];
         $xml = file_get_contents($filename);
-        if (preg_match_all('!<testsuite name="(.*?)" file="(.*?)"!i', $xml, $matches, PREG_SET_ORDER)) {
+        if (preg_match_all('!<testsuite[^<]*?file="(.*?)".*?>!si', $xml, $matches, PREG_SET_ORDER)) {
             foreach ($matches as $m) {
-                list(, $classname, $fileOfUnitTest) = $m;
-                $unitsTests[$fileOfUnitTest] = $classname;
+                list($line, $fileOfUnitTest) = $m;
+
+                $suite = new \stdClass;
+                $suite->file = $fileOfUnitTest;
+
+                $line  = str_ireplace('<testsuite ', '', $line);
+                if (preg_match_all('!(.*?)="(.*?)"!s', $line, $assign, PREG_SET_ORDER)) {
+                    foreach ($assign as $ass) {
+                        $suite->{trim($ass[1])} = trim($ass[2]);
+                    }
+                }
+
+                if (!isset($suite->name, $suite->file, $suite->assertions)) {
+                    // test suite without file
+                    continue;
+                }
+
+                array_push($testsuites, $suite);
             }
         }
 
+
         // analyze each unit test
         // This code is slow and can be optimized
-        foreach ($unitsTests as $filename => $classname) {
+        foreach ($testsuites as $suite) {
             $metricsOfUnitTest = new Metrics();
             $parser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
             $traverser = new \PhpParser\NodeTraverser();
@@ -79,25 +98,31 @@ class UnitTesting
             $traverser->addVisitor(new ClassEnumVisitor($metricsOfUnitTest));
             $traverser->addVisitor(new ExternalsVisitor($metricsOfUnitTest));
 
-            $code = file_get_contents($filename);
+            $code = file_get_contents($suite->file);
             $stmts = $parser->parse($code);
             $traverser->traverse($stmts);
 
-            if (!$metricsOfUnitTest->has($classname)) {
+            if (!$metricsOfUnitTest->has($suite->name)) {
                 continue;
             }
 
             // list of externals sources of unit test
-            $metric = $metricsOfUnitTest->get($classname);
+            $metric = $metricsOfUnitTest->get($suite->name);
             $externals = (array)$metric->get('externals');
 
             // global stats for each test
-            $infoAboutTests[$classname] = (object)[
+            $infoAboutTests[$suite->name] = (object)[
                 'nbExternals' => sizeof(array_unique($externals)),
                 'externals' => array_unique($externals),
                 'filename' => $fileOfUnitTest,
-                'classname' => $classname
+                'classname' => $suite->name,
+                'assertions' => $suite->assertions,
+                'errors' => $suite->errors,
+                'failures' => $suite->failures,
+                'time' => $suite->time,
             ];
+
+            $assertions += $suite->assertions;
 
             foreach ($externals as $external) {
 
@@ -127,12 +152,13 @@ class UnitTesting
             }
         }
 
+        $projectMetric->set('assertions', $assertions);
         $projectMetric->set('tests', $infoAboutTests);
-        $projectMetric->set('nbTests', sizeof($unitsTests));
+        $projectMetric->set('nbSuites', sizeof($testsuites));
         $projectMetric->set('nbCoveredClasses', $nb);
-        $projectMetric->set('percentCoveredClasses', round($nb / $sum * 100, 2));
+        $projectMetric->set('percentCoveredClasses', round($nb / max($sum, 1) * 100, 2));
         $projectMetric->set('nbUncoveredClasses', $sum - $nb);
-        $projectMetric->set('percentUncoveredClasses', round(($sum - $nb) / $sum * 100, 2));
+        $projectMetric->set('percentUncoveredClasses', round(($sum - $nb) / max($sum, 1) * 100, 2));
 
         $metrics->attach($projectMetric);
     }
