9fdac6549f34428296efdf67ea8544b1558a3468
Oskar Pfeifer-Bley <pfeifer-bley@ipa-dguv.de>
2017-04-19 11:21:42 +0200

    Ignore traits for now

--- src/Hal/Metric/Package/PackageCollectingVisitor.php
+++ src/Hal/Metric/Package/PackageCollectingVisitor.php
@@ -33,7 +33,7 @@ class PackageCollectingVisitor extends NodeVisitorAbstract
 
     public function leaveNode(Node $node)
     {
-        if ($node instanceof Class_ || $node instanceof Interface_ || $node instanceof Trait_) {
+        if ($node instanceof Class_ || $node instanceof Interface_) {
             $package = $this->namespace;
 
             $docComment = $node->getDocComment();
