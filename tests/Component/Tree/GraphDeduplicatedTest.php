f3c79f816dcccd7923e228d2f12ffc2ae1bddd67
Vladislav Rastrusny <FractalizeR@yandex.ru>
2017-09-15 11:33:48 +0300

    Preventing duplicate edges from appearing in a graph using Deduplicated graph class. Fixes #291.

---
+++ tests/Component/Tree/GraphDeduplicatedTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Test;
+
+use Hal\Component\Tree\Graph;
+use Hal\Component\Tree\GraphDeduplicated;
+use Hal\Component\Tree\Node;
+
+/**
+ * @group tree
+ */
+class GraphDeduplicatedTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testEdgeDeduplication()
+    {
+        $graph = new GraphDeduplicated();
+        $a = new Node('A');
+        $b = new Node('B');
+        $graph->insert($a);
+        $graph->insert($b);
+
+        $graph->addEdge($a, $b);
+        $graph->addEdge($a, $b);
+        $this->assertCount(1, $graph->getEdges());
+
+        $graph->addEdge($b, $a);
+        $this->assertCount(2, $graph->getEdges());
+    }
+}
