8289946a36ee39ea22a5b439c13df0550e9fa4ff
Oskar Pfeifer-Bley <oskar@programming-php.net>
2018-07-09 18:50:12 +0200

    Use class_alias to load php5 or php7 code
    Fixes #360

---
+++ src/Hal/Component/Ast/Php5NodeTraverser.php
@@ -0,0 +1,27 @@
+<?php
+namespace Hal\Component\Ast;
+
+use PhpParser\Node;
+use PhpParser\NodeTraverser as Mother;
+
+class Php5NodeTraverser extends Mother
+{
+    /** @var Traverser */
+    private $traverser;
+
+    public function __construct($cloneNodes = false, $stopCondition = null)
+    {
+        parent::__construct();
+        $this->traverser = new Traverser($this, $stopCondition);
+    }
+
+    public function traverseNode(Node $node)
+    {
+        return parent::traverseNode($node);
+    }
+
+    protected function traverseArray(array $nodes)
+    {
+        return $this->traverser->traverseArray($nodes, $this->visitors);
+    }
+}
