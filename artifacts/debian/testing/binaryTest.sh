a2fc614ec3f5557c805518537e8e61d387e4bd44
Asis Pattisahusiwa <pattisahusiwa@users.noreply.github.com>
2020-10-03 14:43:42 +0900

    Remove unneeded files

--- artifacts/debian/testing/binaryTest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo $DIR
-function testPharIsNotCorrupted {
-     ${DIR}/../bin/phpmetrics --version|grep "PhpMetrics"
-}
-
-testPharIsNotCorrupted > /dev/null && echo "." ||exit 1
